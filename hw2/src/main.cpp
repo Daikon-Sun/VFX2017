c3c481c8f25f7c33794a2e1cdbb6cb527f70de62
Daikon-Sun <b03901056@ntu.edu.tw>
2017-05-12 20:59:32 +0800

    fix

--- hw2/src/main.cpp
+++ hw2/src/main.cpp
@@ -22,7 +22,7 @@ vector<string> all_blending = {"average", "multi-band"};
 int panorama_mode = 1;
 int detection_mode = 1;
 
-int matching_mode = 1;
+int matching_mode = 2;
 Paras all_matching_para = {{50}, {10, 0.65, 50}, {}};
 vector<int> matching_cnt = {0, 3, 0};
 
@@ -35,7 +35,7 @@ Paras all_stitching_para = {{1000, 2}, {1000, 2}, {1000, 2}, {10}, {5, 2000}};
 vector<int> stitching_cnt = {2, 2, 2, 1, 2};
 
 int blending_mode = 1;
-Paras all_blending_para = {{}, {4}};
+Paras all_blending_para = {{}, {3}};
 vector<int> blending_cnt = {0, 1};
 
 string in_list = "input_images.txt";
